d5c765fd03395404eb233f42983889be317254b3
ananyaas4 <ananyaasgowda@gmail.com>
2025-01-30 16:18:17 +0530

    Home.jsx file Created

---
+++ book-store-app/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () =>{
+    return (
+        <div>
+            <h1>Welcome to Book Store</h1>
+            <p>Explore a wide range of books across various categories.</p>
+            <p>Use the navigation bar to browse books or login to manage your collections.</p>
+        </div>
+    );
+};
+export default Home;
