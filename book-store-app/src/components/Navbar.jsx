1111ee7dc76569b2df24daa7c54f9253cda699f1
ananyaas4 <ananyaasgowda@gmail.com>
2025-01-30 16:17:04 +0530

    Navbar.jsx file Created

---
+++ book-store-app/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+const Navbar = ()=>{
+    const {isAuthenticated, logout} = useContext(AuthContext);
+    return(
+        <nav>
+            <Link to="/">Home</Link>
+            {isAuthenticated && <Link to="/books">Books</Link>}
+            {!isAuthenticated ? <Link to="/login">Login</Link> : <button onClick={logout}>Logout</button>}
+        </nav>
+    )
+}
+export default Navbar;
